367832c073f3733a3edc03c70aa3d08bb2ceebd1
Julia Dohner <jdohner@cs.hmc.edu>
2018-01-28 13:00:35 -0800

    worked with annie sunday

--- Dohner_HW2.m
+++ Dohner_HW2.m
@@ -22,3 +22,64 @@ histogram(z,100,'Normalization','cdf')
 figure
 histogram(z,100,'Normalization','pdf')
 
+
+
+%% Question 3
+
+clear all
+num = 100000;
+nvar=input('Number of random variables to sum ');
+limit = 5*nvar; % just set limits for different cases
+bin=-limit:0.1:limit;
+
+x1=(rand(nvar,num/2)+1)*sqrt(12/nvar);
+x2=(rand(nvar,num/2)-2)*sqrt(12/nvar);
+x = horzcat(x1,x2);
+
+
+% each dataset is a row
+y=sum(x,1); % row vector containing sum of each column
+
+%histogram(y,bin'Normalization','pdf');
+n=hist(y,bin);
+pdfy=n/num/(bin(2)-bin(1));
+bar(bin,pdfy);
+
+
+fprintf('mean = %f\n',mean(y));
+fprintf('2nd moment = %f\n',moment(y,2));
+fprintf('3rd moment = %f\n',moment(y,3));
+fprintf('4th moment = %f\n',moment(y,4));
+fprintf('5th moment = %f\n',moment(y,5));
+fprintf('6th moment = %f\n',moment(y,6));
+
+
+%% Question 4
+
+clear all
+
+% Generate joint-normally-distributed variables x and y such that the mean 
+% of x and y are 0, the variance of x = 2, variance of y = 0.25, and the 
+% mean of x*y = 0.5.
+% Do this by generating two independent normally distributed
+% variables, then scaling and rotating them. 
+
+var_x = 2;
+mean_x = 0; % mean
+x = var_x.*randn(100000,1) + mean_x;
+
+
+var_y = 0.25;
+mean_y = 0; % mean
+y = var_y.*randn(100000,1) + mean_y;
+
+
+% theta=0.5*atan(2*0.5/(1-0.5));
+% xox=sqrt(cos(theta)^2+2*0.5*cos(theta)*sin(theta)+0.5*sin(theta)^2);
+% yox=sqrt(0.5*cos(theta)^2-2*0.5*cos(theta)*sin(theta)+sin(theta)^2);
+% xlim=5;
+% nbin=50;
+
+z = x.*y;
+
+
